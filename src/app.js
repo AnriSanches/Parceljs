501b7f28273f1b23221a1d86998ddc9b5798d089
AnriSanches <89216270+AnriSanches@users.noreply.github.com>
2023-03-13 12:01:52 +0200

    sada

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log('Hello world');
+
+export function add (a,b){
+    return a + b;
+};
+
+console.log(add(10,213));
+console.log(add(10,213));
+console.log(add(10,213));
+console.log(add(12313,213));
